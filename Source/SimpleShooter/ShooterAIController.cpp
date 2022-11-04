be0c232725410ac33f2552aa181b2bd070094b2e
David Brown <81639209+JokingJester@users.noreply.github.com>
2022-11-03 20:05:18 -0500

    Programmed Basic AI Functionality
    The AI will chase the player until it breaks line of sight. It will start chasing the player again if the player is seen again.

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -2,4 +2,33 @@
 
 
 #include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
 
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (LineOfSightTo(PlayerPawn))
+	{
+		MoveToActor(PlayerPawn, 200);
+		SetFocus(PlayerPawn);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+	//if line of sight
+	  // move to player
+	  //set focus
+	//else
+	  //clear focus
+	  //stop movement
+}
